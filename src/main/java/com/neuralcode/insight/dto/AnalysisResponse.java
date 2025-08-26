59bd3cbf994076b41f3621b9aebb49b135127288
Omkar <omkar02d@gmail.com>
2025-08-27 00:48:29 +0530

    Added getters in AnalysisResponse for the testcases and added gitIgnore

--- src/main/java/com/neuralcode/insight/dto/AnalysisResponse.java
+++ src/main/java/com/neuralcode/insight/dto/AnalysisResponse.java
@@ -5,4 +5,15 @@ import java.time.LocalDateTime;
 public record AnalysisResponse(String analysisId, String status, String repositoryUrl, String branchName, LocalDateTime startTime)
 {
 
+    public String getAnalysisId() {
+        return analysisId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getRepositoryUrl() {
+        return repositoryUrl;
+    }
 }
